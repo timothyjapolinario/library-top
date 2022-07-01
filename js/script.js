6beca89663f759d46094806635a55751a8f07d4d
JayJay <timothyjapolinario@gmail.com>
2022-07-01 15:41:01 +0800

    Add functionality of adding dummy books

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let myLibrary = [];
 let bookContainer = document.querySelector(".list-books-container")
 let addBookButton = document.querySelector(".button-add-book");
-
+let bookDescriptionDummy = "Book Description Lorem ipsum,animi maxime obcaecati perferendis impedit laboriosa    m repudiandae quas ipsum quia officiis, repellat quidem! A"
 
 function Book(bookName, bookDescription, hasRead) {
     this.bookName = bookName;
@@ -10,7 +10,7 @@ function Book(bookName, bookDescription, hasRead) {
 }
 
 function createBook(){
-    newBook = new Book("Book1", "Book1Description", false);
+let newBook = new Book("Book1", bookDescriptionDummy , false);
     addBookToLibrary(newBook)
 }
 
@@ -21,9 +21,26 @@ function addBookToLibrary(book) {
 }
 
 function addBookElementUI(book){
-    const para = document.createElement("p")
-    para.innerText = book.bookName;
-    bookContainer.appendChild(para);
+    console.log("lol")
+    let newBook = document.createElement("div")
+    newBook.classList.add("book")
+
+    let newBookName = document.createElement("div");
+    newBookName.classList.add("book-title");
+    newBookName.innerText = book.bookName;
+
+    let newBookDescription = document.createElement("div");
+    newBookDescription.classList.add("book-description");
+    newBookDescription.innerText = book.bookDescription;
+
+    let newBookReadStatus = document.createElement("div")
+    newBookReadStatus.classList.add("book-read-status")
+    newBookReadStatus.innerText = "Read Status: "
+
+    newBook.appendChild(newBookName);
+    newBook.appendChild(newBookDescription);
+    newBook.appendChild(newBookReadStatus);
+    bookContainer.appendChild(newBook);
 }
 
 addBookButton.addEventListener('click', createBook);
